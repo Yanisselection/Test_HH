6fb81d718960929cbdb68074f383b160b61bb1a8
Дмитрий Поляков <electroni_k@mail.ru>
2024-04-10 11:35:18 +0300

    Доделал задачу с классами

--- Тестовое задание.py
+++ Тестовое задание.py
@@ -1,3 +1,6 @@
+import math
+
+
 def find_unique(value: list) -> list:
     """ Функция, которая принимает на вход список целых чисел и возвращает новый список,
     содержащий только уникальные элементы из исходного списка"""
@@ -31,4 +34,31 @@ def sorted_len_word(list_string: list) -> str:
     second = sorted(list_string, key=len, reverse=True)
     return f"По возрастанию {first}\nПо убыванию {second}"
 
-print(sorted_len_word(["apple", "banana", "kiwi", "orange", "grape"]))
+
+class Point:
+    """Класс Point, который представляет собой точку в двумерном пространстве.
+    Класс должен иметь методы для инициализации координат точки, вычисления расстояния до другой точки, а также
+    для получения и изменения координат."""
+    def __init__(self, x_1=0, y_1=0, x_2=0, y_2=0):                                                                     # Инициализация координат
+        self.x_1 = x_1
+        self.y_1 = y_1
+        self.x_2 = x_2
+        self.y_2 = y_2
+
+    def calculate_distance(self):
+        """Вычисление расстояние координат"""
+        return math.sqrt((self.x_2 - self.x_1) ** 2 + (self.y_2 - self.y_1) ** 2)
+
+    def get_coord(self):
+        """Получение координат"""
+        coord_x1_y1 = f"Координаты X_1 - {self.x_1} Y_1 - {self.y_1}"
+        coord_x2_y2 = f"Координаты X_2 - {self.x_2} Y_2 - {self.y_2}"
+        return coord_x1_y1, coord_x2_y2
+
+
+    def set_coord(self, x_1, y_1, x_2, y_2):
+        """Установка координат"""
+        self.x_1 = x_1
+        self.y_1 = y_1
+        self.x_2 = x_2
+        self.y_2 = y_2
